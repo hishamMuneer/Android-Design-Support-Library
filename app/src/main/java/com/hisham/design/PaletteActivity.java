6ac3db268433ec9a46c2f7115315cac60184caa4
Hisham <hisham.dbd@gmail.com>
2015-06-11 18:20:13 +0530

    Compressed images.

--- app/src/main/java/com/hisham/design/PaletteActivity.java
+++ app/src/main/java/com/hisham/design/PaletteActivity.java
@@ -19,7 +19,7 @@ import java.util.Random;
 public class PaletteActivity extends AppCompatActivity {
 
     private int images[] = new int[]{R.drawable.i1, R.drawable.i2, R.drawable.i3, R.drawable.i4,
-            R.drawable.i5, R.drawable.i6, R.drawable.i7, R.drawable.i8, R.drawable.i9, R.drawable.i10, R.drawable.koala, R.drawable.tulips};
+            R.drawable.i5, R.drawable.i6, R.drawable.i7, R.drawable.i10, R.drawable.koala, R.drawable.tulips};
     private TextView tvLabel;
     private ImageView imageView;
     private View relativeParent;
